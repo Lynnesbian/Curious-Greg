3d03569107498dc16a5705d1ee7755bc3c9e9dd4
Lynne <thecoolone3@gmail.com>
2018-11-02 00:25:47 +1000

    create files, table, config, etc

---
+++ login.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#Curious Greg - Curious Cat to Mastodon crossposter
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import requests, sqlite3, json, argparse
+from mastodon import Mastodon
+
+db = sqlite3.connect("database.db")
+c = db.cursor()
+
+c.execute("CREATE TABLE IF NOT EXISTS `data` (username VARCHAR NOT NULL, secret VARCHAR NOT NULL, latest_post VARCHAR)")
+try:
+	client_auth = json.load(open("auth.json"))
+except Exception: #todo: only handle file not found
+	print("Couldn't load auth.json, are you sure you created it?")
+
